176e7583981c70eb636a14e016cd6f1fce7e4211
Maximilian Rogers <maxroger@amazon.com>
2021-08-25 00:13:42 -0400

    adding explicit platform support

--- Package.swift
+++ Package.swift
@@ -3,15 +3,21 @@
 import PackageDescription
 
 let package = Package(
-  name: "xctest-dynamic-overlay",
-  products: [
-    .library(name: "XCTestDynamicOverlay", targets: ["XCTestDynamicOverlay"])
-  ],
-  targets: [
-    .target(name: "XCTestDynamicOverlay"),
-    .testTarget(
-      name: "XCTestDynamicOverlayTests",
-      dependencies: ["XCTestDynamicOverlay"]
-    ),
-  ]
+    name: "xctest-dynamic-overlay",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(name: "XCTestDynamicOverlay", targets: ["XCTestDynamicOverlay"])
+    ],
+    targets: [
+        .target(name: "XCTestDynamicOverlay"),
+        .testTarget(
+            name: "XCTestDynamicOverlayTests",
+            dependencies: ["XCTestDynamicOverlay"]
+        ),
+    ]
 )
