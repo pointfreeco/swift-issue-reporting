cf36c3aeb3edd71b816aa5569f434b7acbb1d30c
stephencelis <stephencelis@users.noreply.github.com>
2022-06-28 18:57:59 +0000

    Run swift-format

--- Sources/XCTestDynamicOverlay/Internal/XCTCurrentTestCase.swift
+++ Sources/XCTestDynamicOverlay/Internal/XCTCurrentTestCase.swift
@@ -5,14 +5,14 @@
     var XCTCurrentTestCase: AnyObject? {
       guard
         let XCTestObservationCenter = NSClassFromString("XCTestObservationCenter")
-        as Any as? NSObjectProtocol,
+          as Any as? NSObjectProtocol,
         String(describing: XCTestObservationCenter) != "<null>",
         let shared = XCTestObservationCenter.perform(Selector(("sharedTestObservationCenter")))?
           .takeUnretainedValue(),
         let observers = shared.perform(Selector(("observers")))?
           .takeUnretainedValue() as? [AnyObject],
         let observer =
-        observers
+          observers
           .first(where: { NSStringFromClass(type(of: $0)) == "XCTestMisuseObserver" }),
         let currentTestCase = observer.perform(Selector(("currentTestCase")))?
           .takeUnretainedValue()
