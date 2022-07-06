63136238e6e49b6ff25cf5d53dbd4e645ed43ae5
Stephen Celis <stephen@stephencelis.com>
2022-07-06 14:19:47 -0400

    wip

--- Sources/XCTestDynamicOverlay/XCTIsTesting.swift
+++ Sources/XCTestDynamicOverlay/XCTIsTesting.swift
@@ -1,8 +1,9 @@
 import Foundation
 
 public let _XCTIsTesting: Bool = {
-  ProcessInfo.processInfo.arguments.first
-    .flatMap(URL.init(fileURLWithPath:))
-    .map { $0.lastPathComponent == "xctest" || $0.pathExtension == "xctest" }
-  ?? ProcessInfo.processInfo.environment.keys.contains("XCTestSessionIdentifier")
+  ProcessInfo.processInfo.environment.keys.contains("XCTestSessionIdentifier")
+    || ProcessInfo.processInfo.arguments.first
+      .flatMap(URL.init(fileURLWithPath:))
+      .map { $0.lastPathComponent == "xctest" || $0.pathExtension == "xctest" }
+      ?? false
 }()
