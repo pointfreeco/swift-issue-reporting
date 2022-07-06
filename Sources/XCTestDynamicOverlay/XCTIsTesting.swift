046aba2944a7e197e5dc070836f8fe2d47d0e471
Stephen Celis <stephen@stephencelis.com>
2022-07-06 14:27:49 -0400

    Make `_XCTIsTesting` check a bit more robust (#21)
    * Improve _XCTIsTesting
    
    * wip

--- Sources/XCTestDynamicOverlay/XCTIsTesting.swift
+++ Sources/XCTestDynamicOverlay/XCTIsTesting.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 public let _XCTIsTesting: Bool = {
-  guard let path = ProcessInfo.processInfo.arguments.first
-  else { return false }
-
-  let url = URL(fileURLWithPath: path)
-  return url.lastPathComponent == "xctest" || url.pathExtension == "xctest"
+  ProcessInfo.processInfo.environment.keys.contains("XCTestSessionIdentifier")
+    || ProcessInfo.processInfo.arguments.first
+      .flatMap(URL.init(fileURLWithPath:))
+      .map { $0.lastPathComponent == "xctest" || $0.pathExtension == "xctest" }
+      ?? false
 }()
