ead64bc022137ccb3b87e8122ba66d54838bffb0
stephencelis <stephencelis@users.noreply.github.com>
2022-09-14 15:14:26 +0000

    Run swift-format

--- Sources/XCTestDynamicOverlay/XCTIsTesting.swift
+++ Sources/XCTestDynamicOverlay/XCTIsTesting.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 #if !os(WASI)
-public let _XCTIsTesting: Bool = {
-  ProcessInfo.processInfo.environment.keys.contains("XCTestSessionIdentifier")
-    || ProcessInfo.processInfo.arguments.first
-      .flatMap(URL.init(fileURLWithPath:))
-      .map { $0.lastPathComponent == "xctest" || $0.pathExtension == "xctest" }
-      ?? false
-}()
+  public let _XCTIsTesting: Bool = {
+    ProcessInfo.processInfo.environment.keys.contains("XCTestSessionIdentifier")
+      || ProcessInfo.processInfo.arguments.first
+        .flatMap(URL.init(fileURLWithPath:))
+        .map { $0.lastPathComponent == "xctest" || $0.pathExtension == "xctest" }
+        ?? false
+  }()
 #endif
