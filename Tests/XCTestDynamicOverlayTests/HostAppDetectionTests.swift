8822967ae18dad92af16534326b99ce7fa50feb6
Stephen Celis <stephen@stephencelis.com>
2023-02-06 14:54:25 -0800

    Finesse host application warning
    Just a lil cleanup work from #48.

--- Tests/XCTestDynamicOverlayTests/HostAppDetectionTests.swift
+++ Tests/XCTestDynamicOverlayTests/HostAppDetectionTests.swift
@@ -1,43 +1,23 @@
-import XCTest
+#if DEBUG
+  import XCTest
 
-@testable import XCTestDynamicOverlay
+  @testable import XCTestDynamicOverlay
 
-final class HostAppCallStackTests: XCTestCase {
-  func testIsAbleToDetectTest() {
-    XCTAssertEqual(
-      testCaseSubclass(callStackSymbols: Thread.callStackSymbols).map(ObjectIdentifier.init),
-      ObjectIdentifier(HostAppCallStackTests.self)
-    )
-  }
-
-  func testIsAbleToDetectAsyncTest() async {
-    XCTAssertEqual(
-      testCaseSubclass(callStackSymbols: Thread.callStackSymbols).map(ObjectIdentifier.init),
-      ObjectIdentifier(HostAppCallStackTests.self)
-    )
-  }
-
-  func testIsAbleToDetectThrowingTest() throws {
-    XCTAssertEqual(
-      testCaseSubclass(callStackSymbols: Thread.callStackSymbols).map(ObjectIdentifier.init),
-      ObjectIdentifier(HostAppCallStackTests.self)
-    )
-  }
-
-  func testIsAbleToDetectAsyncThrowingTest() async throws {
-    XCTAssertEqual(
-      testCaseSubclass(callStackSymbols: Thread.callStackSymbols).map(ObjectIdentifier.init),
-      ObjectIdentifier(HostAppCallStackTests.self)
-    )
-  }
-
-  #if !os(Linux)
-    func testFailDoesNotAppendHostAppWarningFromATest() {
-      XCTExpectFailure {
-        XCTestDynamicOverlay.XCTFail("foo")
-      } issueMatcher: {
-        $0.compactDescription == "foo"
-      }
+  final class HostAppCallStackTests: XCTestCase {
+    func testIsAbleToDetectTest() {
+      XCTAssert(Thread.callStackSymbols.contains(where: isTestFrame))
     }
-  #endif
-}
+
+    func testIsAbleToDetectAsyncTest() async {
+      XCTAssert(Thread.callStackSymbols.contains(where: isTestFrame))
+    }
+
+    func testIsAbleToDetectThrowingTest() throws {
+      XCTAssert(Thread.callStackSymbols.contains(where: isTestFrame))
+    }
+
+    func testIsAbleToDetectAsyncThrowingTest() async throws {
+      XCTAssert(Thread.callStackSymbols.contains(where: isTestFrame))
+    }
+  }
+#endif
