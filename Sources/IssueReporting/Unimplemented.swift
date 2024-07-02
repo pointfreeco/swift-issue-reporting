4c1e1299174e34810c16a93dc08b436ff675e18a
Brandon Williams <mbrandonw@hey.com>
2024-07-02 15:07:20 -0400

    async versions of unimplemented

--- Sources/IssueReporting/Unimplemented.swift
+++ Sources/IssueReporting/Unimplemented.swift
@@ -13,12 +13,40 @@ public func unimplemented<each A, Result>(
 }
 
 @Sendable
+public func unimplemented<each A, Result>(
+  _ description: @autoclosure @escaping @Sendable () -> String = "",
+  placeholder: @autoclosure @escaping @Sendable () -> Result = (),
+  fileID: StaticString = #fileID,
+  line: UInt = #line,
+  function: StaticString = #function
+) -> @Sendable (repeat each A) async -> Result {
+  return { (arg: repeat each A) in
+    _fail(description(), (repeat each arg), fileID: fileID, line: line, function: function)
+    return placeholder()
+  }
+}
+
+@Sendable
 public func throwing<each A, Result>(
   _ description: @autoclosure @escaping @Sendable () -> String = "",
   fileID: StaticString = #fileID,
   line: UInt = #line,
   function: StaticString = #function
 ) -> @Sendable (repeat each A) throws -> Result {
+  return { (arg: repeat each A) in
+    let description = description()
+    _fail(description, (repeat each arg), fileID: fileID, line: line, function: function)
+    throw UnimplementedFailure(description: description)
+  }
+}
+
+@Sendable
+public func throwing<each A, Result>(
+  _ description: @autoclosure @escaping @Sendable () -> String = "",
+  fileID: StaticString = #fileID,
+  line: UInt = #line,
+  function: StaticString = #function
+) -> @Sendable (repeat each A) async throws -> Result {
   return { (arg: repeat each A) in
     let description = description()
     _fail(description, (repeat each arg), fileID: fileID, line: line, function: function)
