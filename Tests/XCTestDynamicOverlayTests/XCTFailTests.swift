bcd911908640a310f0e80239a90910d9712e0015
Stephen Celis <stephen@stephencelis.com>
2024-07-10 11:53:35 -0700

    wip

--- Tests/XCTestDynamicOverlayTests/XCTFailTests.swift
+++ Tests/XCTestDynamicOverlayTests/XCTFailTests.swift
@@ -1,9 +1,9 @@
-//import XCTest
-//
-//final class XCTFailTests: XCTestCase {
-//  func testXCTFailShouldFail() async throws {
-//    if ProcessInfo.processInfo.environment["TEST_FAILURE"] != nil {
-//      MyXCTFail("This is expected to fail!")
-//    }
-//  }
-//}
+import XCTest
+
+final class XCTFailTests: XCTestCase {
+  func testXCTFailShouldFail() async throws {
+    if ProcessInfo.processInfo.environment["TEST_FAILURE"] != nil {
+      MyXCTFail("This is expected to fail!")
+    }
+  }
+}
