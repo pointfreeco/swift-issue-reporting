c6a39c913d0924b0b302618b18dda4e938f48e70
Stephen Celis <stephen@stephencelis.com>
2024-07-18 13:43:00 -0700

    wip

--- Sources/IssueReporting/Internal/XCTest.swift
+++ Sources/IssueReporting/Internal/XCTest.swift
@@ -16,27 +16,31 @@ func _XCTFail(
   file: StaticString = #filePath,
   line: UInt = #line
 ) {
-  var pointer: UnsafeMutableRawPointer? {
-    let symbol = "$s6XCTest7XCTFail_4file4lineySS_s12StaticStringVSutF"
-    #if canImport(Darwin) || canImport(Glibc)
-      return dlsym(dlopen("libXCTest.so", RTLD_NOW) ?? dlopen(nil, RTLD_NOW), symbol)
-    #elseif canImport(WinSDK)
-      guard let dll = LoadLibraryA("XCTest.dll") else { return nil }
-      return GetProcAddress(dll, symbol)
-    #else
-      return nil
-    #endif
-  }
+  #if DEBUG
+    var pointer: UnsafeMutableRawPointer? {
+      let symbol = "$s6XCTest7XCTFail_4file4lineySS_s12StaticStringVSutF"
+      #if canImport(Darwin) || canImport(Glibc)
+        return dlsym(dlopen("libXCTest.so", RTLD_NOW) ?? dlopen(nil, RTLD_NOW), symbol)
+      #elseif canImport(WinSDK)
+        guard let dll = LoadLibraryA("XCTest.dll") else { return nil }
+        return GetProcAddress(dll, symbol)
+      #else
+        return nil
+      #endif
+    }
 
-  guard
-    !_XCTExpectedFailure.isInFailingBlock,
-    let pointer
-  else { return }
-  let XCTFail = unsafeBitCast(
-    pointer,
-    to: (@convention(thin) (String, StaticString, UInt) -> Void).self
-  )
-  XCTFail(message, file, line)
+    guard
+      !_XCTExpectedFailure.isInFailingBlock,
+      let pointer
+    else { return }
+    let XCTFail = unsafeBitCast(
+      pointer,
+      to: (@convention(thin) (String, StaticString, UInt) -> Void).self
+    )
+    XCTFail(message, file, line)
+  #else
+    // TODO: Warn
+  #endif
 }
 
 @_transparent
@@ -49,45 +53,50 @@ func _XCTExpectFailure<R>(
   line: UInt,
   failingBlock: () throws -> R
 ) rethrows -> R {
-  guard enabled != false
-  else {
-    return try failingBlock()
-  }
-  #if _runtime(_ObjC)
-    guard
-      let xctExpectFailureInBlockPtr = dlsym(
-        dlopen(nil, RTLD_NOW),
-        "XCTExpectFailureWithOptionsInBlock"
-      ),
-      let xctExpectedFailureOptions = NSClassFromString("XCTExpectedFailureOptions")
-        as Any as? NSObjectProtocol,
-      let options = strict ?? true
-        ? xctExpectedFailureOptions
-          .perform(NSSelectorFromString("alloc"))?.takeUnretainedValue()
-          .perform(NSSelectorFromString("init"))?.takeUnretainedValue()
-        : xctExpectedFailureOptions
-          .perform(NSSelectorFromString("nonStrictOptions"))?.takeUnretainedValue()
+  #if DEBUG
+    guard enabled != false
     else {
       return try failingBlock()
     }
-    let xctExpectFailureInBlock = unsafeBitCast(
-      xctExpectFailureInBlockPtr,
-      to: (@convention(c) (String?, AnyObject, () -> Void) -> Void).self
-    )
-    var result: Result<R, any Error>!
-    xctExpectFailureInBlock(failureReason, options) {
-      result = Result { try failingBlock() }
-    }
-    return try result._rethrowGet()
+    #if _runtime(_ObjC)
+      guard
+        let xctExpectFailureInBlockPtr = dlsym(
+          dlopen(nil, RTLD_NOW),
+          "XCTExpectFailureWithOptionsInBlock"
+        ),
+        let xctExpectedFailureOptions = NSClassFromString("XCTExpectedFailureOptions")
+          as Any as? NSObjectProtocol,
+        let options = strict ?? true
+          ? xctExpectedFailureOptions
+            .perform(NSSelectorFromString("alloc"))?.takeUnretainedValue()
+            .perform(NSSelectorFromString("init"))?.takeUnretainedValue()
+          : xctExpectedFailureOptions
+            .perform(NSSelectorFromString("nonStrictOptions"))?.takeUnretainedValue()
+      else {
+        return try failingBlock()
+      }
+      let xctExpectFailureInBlock = unsafeBitCast(
+        xctExpectFailureInBlockPtr,
+        to: (@convention(c) (String?, AnyObject, () -> Void) -> Void).self
+      )
+      var result: Result<R, any Error>!
+      xctExpectFailureInBlock(failureReason, options) {
+        result = Result { try failingBlock() }
+      }
+      return try result._rethrowGet()
+    #else
+      _XCTFail(
+        "XCTest's XCTExpectFailure is unavailable on this platform.",
+        file: file,
+        line: line
+      )
+      return try _XCTExpectedFailure.$isInFailingBlock.withValue(true) {
+        try failingBlock()
+      }
+    #endif
   #else
-    _XCTFail(
-      "XCTest's XCTExpectFailure is unavailable on this platform.",
-      file: file,
-      line: line
-    )
-    return try _XCTExpectedFailure.$isInFailingBlock.withValue(true) {
-      try failingBlock()
-    }
+    // TODO: Warn
+    try failingBlock()
   #endif
 }
 
