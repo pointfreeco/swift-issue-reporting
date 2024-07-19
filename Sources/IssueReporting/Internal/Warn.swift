ed21414d5300c4df5f35630598254ac29632fe48
Stephen Celis <stephen@stephencelis.com>
2024-07-19 14:22:45 -0700

    wip

--- Sources/IssueReporting/Internal/Warn.swift
+++ Sources/IssueReporting/Internal/Warn.swift
@@ -6,10 +6,5 @@ import Foundation
 
 @usableFromInline
 func printError(_ message: String) {
-//  #if os(Linux) || os(macOS)
-    fputs("\(message)\n", stderr)
-//  #else
-//    // TODO: Print to `stderr` on Windows?
-//    print(message)
-//  #endif
+  fputs("\(message)\n", stderr)
 }
