185c307f143507f001aa6f3c3a41bae898263506
Stephen Celis <stephen@stephencelis.com>
2024-08-08 10:00:30 -0700

    wip

--- Sources/IssueReporting/Internal/Deprecations.swift
+++ Sources/IssueReporting/Internal/Deprecations.swift
@@ -1,10 +1,12 @@
 // NB: Deprecated after 1.2.2
 
-@available(*, unavailable, renamed: "_FatalErrorReporter")
-public typealias BreakpointReporter = _BreakpointReporter
+#if canImport(Darwin)
+  @available(*, unavailable, renamed: "_BreakpointReporter")
+  public typealias BreakpointReporter = _BreakpointReporter
+#endif
 
 @available(*, unavailable, renamed: "_FatalErrorReporter")
 public typealias FatalErrorReporter = _FatalErrorReporter
 
-@available(*, unavailable, renamed: "_FatalErrorReporter")
+@available(*, unavailable, renamed: "_RuntimeWarningReporter")
 public typealias RuntimeWarningReporter = _RuntimeWarningReporter
