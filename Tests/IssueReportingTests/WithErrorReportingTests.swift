ae96d368e6df11ac9dd968787847193c8d732444
Brandon Williams <mbrandonw@hey.com>
2025-01-29 12:02:14 -0800

    add test

---
+++ Tests/IssueReportingTests/WithErrorReportingTests.swift
@@ -0,0 +1,19 @@
+#if canImport(Testing)
+  import Testing
+  import IssueReporting
+
+  @Suite
+  struct WithErrorReportingTests {
+    @Test func basics() {
+      withKnownIssue {
+        withErrorReporting {
+          throw SomeError()
+        }
+      } matching: { issue in
+        issue.description == "Caught error: SomeError()"
+      }
+    }
+  }
+
+  private struct SomeError: Error {}
+#endif
