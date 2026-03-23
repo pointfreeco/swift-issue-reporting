39e1d60d5b4832529ea893794b9020e46e34e874
James Jackson <james.sorley.jackson@gmail.com>
2026-03-22 21:46:07 -0400

    fix: change package access to public for broader build compatibility

--- Sources/IssueReportingPackageSupport/_Test.swift
+++ Sources/IssueReportingPackageSupport/_Test.swift
@@ -1,13 +1,8 @@
-@usableFromInline
-package struct _Test {
-  @usableFromInline
-  package let id: AnyHashable
+public struct _Test {
+  public let id: AnyHashable
+  public let traits: [any Sendable]
 
-  @usableFromInline
-  package let traits: [any Sendable]
-
-  @usableFromInline
-  package init(id: AnyHashable, traits: [any Sendable]) {
+  public init(id: AnyHashable, traits: [any Sendable]) {
     self.id = id
     self.traits = traits
   }
