13d6d8a34a52b6cb249e7732689e7e49a2392b7f
Stephen Celis <stephen@stephencelis.com>
2026-07-06 13:12:16 -0700

    `unimplemented`: Support `sending` results (#192)
    Because `nonisolated` generates empty/throwing closures, the `@Sendable`
    requirement gracefully erases to non-sendable and nonisolated-nonsending
    parameters, but `sending` results are not currently supported.
    
    This branch lifts that restriction at the cost of dropping Swift 5.9
    support. But Swift 5.9 is over two and a half years old at this point,
    and we've dropped it from many other projects, so let's drop it here.

--- Sources/IssueReporting/ErrorReporting.swift
+++ Sources/IssueReporting/ErrorReporting.swift
@@ -86,48 +86,31 @@ public func withErrorReporting<R>(
   ) as R??) ?? nil
 }
 
-#if compiler(>=6)
-  /// Evaluates a throwing closure and automatically catches and reports any error thrown.
-  ///
-  /// - Parameters:
-  ///   - message: A message describing the expectation.
-  ///   - reporters: Issue reporters to notify during the operation.
-  ///   - fileID: The source `#fileID` associated with the error reporting.
-  ///   - filePath: The source `#filePath` associated with the error reporting.
-  ///   - line: The source `#line` associated with the error reporting.
-  ///   - column: The source `#column` associated with the error reporting.
-  ///   - isolation: The isolation associated with the error reporting.
-  ///   - body: An asynchronous operation.
-  /// - Returns: The optional result of the operation, or `nil` if an error was thrown.
-  @_transparent
-  public func withErrorReporting<R>(
-    _ message: @autoclosure () -> String? = nil,
-    to reporters: [any IssueReporter]? = nil,
-    fileID: StaticString = #fileID,
-    filePath: StaticString = #filePath,
-    line: UInt = #line,
-    column: UInt = #column,
-    isolation: isolated (any Actor)? = #isolation,
-    catching body: () async throws -> sending R
-  ) async -> R? {
-    if let reporters {
-      return await withIssueReporters(reporters) {
-        do {
-          return try await body()
-        } catch is CancellationError {
-        } catch {
-          reportIssue(
-            error,
-            message(),
-            fileID: fileID,
-            filePath: filePath,
-            line: line,
-            column: column
-          )
-        }
-        return nil
-      }
-    } else {
+/// Evaluates a throwing closure and automatically catches and reports any error thrown.
+///
+/// - Parameters:
+///   - message: A message describing the expectation.
+///   - reporters: Issue reporters to notify during the operation.
+///   - fileID: The source `#fileID` associated with the error reporting.
+///   - filePath: The source `#filePath` associated with the error reporting.
+///   - line: The source `#line` associated with the error reporting.
+///   - column: The source `#column` associated with the error reporting.
+///   - isolation: The isolation associated with the error reporting.
+///   - body: An asynchronous operation.
+/// - Returns: The optional result of the operation, or `nil` if an error was thrown.
+@_transparent
+public func withErrorReporting<R>(
+  _ message: @autoclosure () -> String? = nil,
+  to reporters: [any IssueReporter]? = nil,
+  fileID: StaticString = #fileID,
+  filePath: StaticString = #filePath,
+  line: UInt = #line,
+  column: UInt = #column,
+  isolation: isolated (any Actor)? = #isolation,
+  catching body: () async throws -> sending R
+) async -> R? {
+  if let reporters {
+    return await withIssueReporters(reporters) {
       do {
         return try await body()
       } catch is CancellationError {
@@ -143,107 +126,54 @@ public func withErrorReporting<R>(
       }
       return nil
     }
-  }
-
-  /// Evaluates a throwing closure and automatically catches and reports any error thrown.
-  ///
-  /// - Parameters:
-  ///   - message: A message describing the expectation.
-  ///   - reporters: Issue reporters to notify during the operation.
-  ///   - fileID: The source `#fileID` associated with the error reporting.
-  ///   - filePath: The source `#filePath` associated with the error reporting.
-  ///   - line: The source `#line` associated with the error reporting.
-  ///   - column: The source `#column` associated with the error reporting.
-  ///   - isolation: The isolation associated with the error reporting.
-  ///   - body: An asynchronous operation.
-  /// - Returns: The optional result of the operation, or `nil` if an error was thrown.
-  @_transparent
-  public func withErrorReporting<R>(
-    _ message: @autoclosure () -> String? = nil,
-    to reporters: [any IssueReporter]? = nil,
-    fileID: StaticString = #fileID,
-    filePath: StaticString = #filePath,
-    line: UInt = #line,
-    column: UInt = #column,
-    isolation: isolated (any Actor)? = #isolation,
-    catching body: () async throws -> sending R?
-  ) async -> R? {
-    (await withErrorReporting(
-      message(),
-      to: reporters,
-      fileID: fileID,
-      filePath: filePath,
-      line: line,
-      column: column,
-      catching: body
-    ) as R??) ?? nil
-  }
-#else
-  @_transparent
-  @_unsafeInheritExecutor
-  public func withErrorReporting<R>(
-    _ message: @autoclosure () -> String? = nil,
-    to reporters: [any IssueReporter]? = nil,
-    fileID: StaticString = #fileID,
-    filePath: StaticString = #filePath,
-    line: UInt = #line,
-    column: UInt = #column,
-    catching body: () async throws -> R
-  ) async -> R? {
-    if let reporters {
-      return await withIssueReporters(reporters) {
-        do {
-          return try await body()
-        } catch is CancellationError {
-        } catch {
-          reportIssue(
-            error,
-            message(),
-            fileID: fileID,
-            filePath: filePath,
-            line: line,
-            column: column
-          )
-        }
-        return nil
-      }
-    } else {
-      do {
-        return try await body()
-      } catch is CancellationError {
-      } catch {
-        reportIssue(
-          error,
-          message(),
-          fileID: fileID,
-          filePath: filePath,
-          line: line,
-          column: column
-        )
-      }
-      return nil
+  } else {
+    do {
+      return try await body()
+    } catch is CancellationError {
+    } catch {
+      reportIssue(
+        error,
+        message(),
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+      )
     }
+    return nil
   }
+}
 
-  @_transparent
-  @_unsafeInheritExecutor
-  public func withErrorReporting<R>(
-    _ message: @autoclosure () -> String? = nil,
-    to reporters: [any IssueReporter]? = nil,
-    fileID: StaticString = #fileID,
-    filePath: StaticString = #filePath,
-    line: UInt = #line,
-    column: UInt = #column,
-    catching body: () async throws -> R?
-  ) async -> R? {
-    (await withErrorReporting(
-      message(),
-      to: reporters,
-      fileID: fileID,
-      filePath: filePath,
-      line: line,
-      column: column,
-      catching: body
-    ) as R??) ?? nil
-  }
-#endif
+/// Evaluates a throwing closure and automatically catches and reports any error thrown.
+///
+/// - Parameters:
+///   - message: A message describing the expectation.
+///   - reporters: Issue reporters to notify during the operation.
+///   - fileID: The source `#fileID` associated with the error reporting.
+///   - filePath: The source `#filePath` associated with the error reporting.
+///   - line: The source `#line` associated with the error reporting.
+///   - column: The source `#column` associated with the error reporting.
+///   - isolation: The isolation associated with the error reporting.
+///   - body: An asynchronous operation.
+/// - Returns: The optional result of the operation, or `nil` if an error was thrown.
+@_transparent
+public func withErrorReporting<R>(
+  _ message: @autoclosure () -> String? = nil,
+  to reporters: [any IssueReporter]? = nil,
+  fileID: StaticString = #fileID,
+  filePath: StaticString = #filePath,
+  line: UInt = #line,
+  column: UInt = #column,
+  isolation: isolated (any Actor)? = #isolation,
+  catching body: () async throws -> sending R?
+) async -> R? {
+  (await withErrorReporting(
+    message(),
+    to: reporters,
+    fileID: fileID,
+    filePath: filePath,
+    line: line,
+    column: column,
+    catching: body
+  ) as R??) ?? nil
+}
